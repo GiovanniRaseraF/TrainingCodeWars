c7f38adebae87506bb1f6e733a72cd4ff7578b4a
MIT <giovannirasera3@gmail.com>
2020-09-23 20:12:18 +0200

    Push dal mac

---
+++ Stream09/JosephusPermutation.cpp
@@ -0,0 +1,20 @@
+#include <utility>
+#include <vector>
+#include <numeric>
+#include <string>
+#include <functional>
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+vector<int> josephus(vector<int> items, int k) {
+	vector<int> ret{};
+	
+	return ret;
+}
+
+int main() {
+	josephus({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 3);
+}
+
