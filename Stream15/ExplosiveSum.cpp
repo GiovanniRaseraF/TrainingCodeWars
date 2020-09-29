c2927530afa7ebf4f583bf757ffb7d93c92a4f53
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2020-09-30 00:09:57 +0200

    Streaming 15

---
+++ Stream15/ExplosiveSum.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <numeric>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <queue>
+#include <climits>
+#include <iostream>
+
+using namespace std;
+
+using ull = long long;
+
+ull exp_sum_p(ull n, ull m) {
+	if (n <= 0) return 0;
+	if (n == 1) return 1;
+
+	ull sum = 0;
+	for (ull k = 1; k <= m; k++) {
+		sum += exp_sum_p(n-m, k);
+	}
+
+	return sum;
+}
+
+ull exp_sum(ull n) {
+	ull sum = 0;
+	for (ull m = 1; m <= n; m++) {
+		sum += exp_sum_p(n, m);
+	}
+	return sum;
+}
+
+int main() {
+	for (int i = 0; i <= 10; i++) {
+		cout << "i: " << i << " -> " << exp_sum(i) << endl;
+	}
+	return 0;
+}
