64c487c94f1e19af1ca7b3d37240054795dd522b
Giovanni Rasera <giovanni.raserafarra@gmail.com>
2020-10-11 14:50:27 +0200

    Streaming 22 23

---
+++ Stream23/UpdatableObservable.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <functional>
+#include <mutex>
+#include <assert.h>
+
+using namespace std;
+
+class updatable_observable {
+public:
+	virtual void update_me(int& updater) {}
+};
